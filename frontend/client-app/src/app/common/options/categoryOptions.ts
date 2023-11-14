e594f8acc7c0b6250b81bed569b212be844c6c4a
Alex Lux <alexlux@live.com>
2023-11-14 13:39:22 -0800

    creating reusable text/text area/select input

---
+++ frontend/client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,9 @@
+export const categoryOptions = [
+  { text: "Drinks", value: "drinks" },
+  { text: "Food", value: "food" },
+  { text: "Dessert", value: "dessert" },
+  { text: "Other", value: "other" },
+  { text: "Film", value: "film" },
+  { text: "Culture", value: "culture" },
+  { text: "Travel", value: "travel" },
+];
--- frontend/client-app/src/components/activities/form/ActivityForm.tsx
+++ frontend/client-app/src/components/activities/form/ActivityForm.tsx
@@ -1,26 +1,24 @@
-import { ChangeEvent, useEffect, useState } from "react";
-import { Button, FormField, Label, Segment } from "semantic-ui-react";
+import { useEffect, useState } from "react";
+import { Button, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 // import { v4 as uuid } from "uuid";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form } from "formik";
 import * as Yup from "yup";
+import MyTextInput from "../../../app/common/form/MyTextInput";
+import MyTextArea from "./MyTextArea";
+import MySelectInput from "./MySelectInput";
+import { categoryOptions } from "../../../app/common/options/categoryOptions";
 
 const ActivityForm = () => {
   const { activityStore } = useStore();
-  const {
-    createActivity,
-    updateActivity,
-    loading,
-    loadActivity,
-    loadingInitial,
-  } = activityStore;
+  const { loading, loadActivity, loadingInitial } = activityStore;
 
   const { id } = useParams<{ id: string }>();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const [activity, setActivity] = useState<Activity>({
     id: "",
@@ -34,6 +32,11 @@ const ActivityForm = () => {
 
   const validationSchema = Yup.object({
     title: Yup.string().required("The activity title is required"),
+    description: Yup.string().required("The activity description is required"),
+    category: Yup.string().required("The activity category is required"),
+    date: Yup.string().required("The activity date is required").nullable(),
+    venue: Yup.string().required("The activity venue is required"),
+    city: Yup.string().required("The activity city is required"),
   });
 
   useEffect(() => {
@@ -72,19 +75,24 @@ const ActivityForm = () => {
       >
         {({ handleSubmit }) => (
           <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
-            <FormField>
+            {/* <FormField>
               <Field placeholder="Title" name="title" />
               <ErrorMessage
                 name="title"
                 render={(error) => <Label basic color="red" content={error} />}
               />
-            </FormField>
-            <Field placeholder="Title" name="title" />
-            <Field placeholder="Description" name="description" />
-            <Field placeholder="Category" name="category" />
-            <Field type="date" placeholder="Date" name="date" />
-            <Field placeholder="City" name="city" />
-            <Field placeholder="Venue" name="venue" />
+            </FormField> */}
+            <MyTextInput name="title" placeholder="Title" />
+            <MyTextInput placeholder="Title" name="title" />
+            <MyTextArea rows={3} placeholder="Description" name="description" />
+            <MySelectInput
+              placeholder="Category"
+              name="category"
+              options={categoryOptions}
+            />
+            <MyTextInput type="date" placeholder="Date" name="date" />
+            <MyTextInput placeholder="City" name="city" />
+            <MyTextInput placeholder="Venue" name="venue" />
             <Button
               loading={loading}
               floated="right"
