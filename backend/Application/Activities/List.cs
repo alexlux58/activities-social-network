b18db852cf96ad018982ef7295e6a2ebc3e9c8fe
Alex Lux <alexlux@live.com>
2023-11-29 21:55:54 -0800

    update create activity handler, loading related data

--- backend/Application/Activities/List.cs
+++ backend/Application/Activities/List.cs
@@ -34,7 +34,12 @@ namespace Application.Activities
                 //     _logger.LogInformation("Task was cancelled");
                 // }
 
-                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
+                var activities = await _context.Activities
+                    .Include(a => a.Attendees)
+                    .ThenInclude(u => u.AppUser)
+                    .ToListAsync(cancellationToken);
+
+                return Result<List<Activity>>.Success(activities);
             }
 
 
