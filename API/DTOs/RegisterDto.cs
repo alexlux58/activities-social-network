b552d14767982d43f5e0c467cacf4a098e7a7fec
Alex Lux <alexlux@live.com>
2024-01-12 17:46:12 -0800

    pre-deploy

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,7 +8,7 @@ namespace API.DTOs
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$", ErrorMessage = "Password must be complex")]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{4,}$", ErrorMessage = "Password must be complex")]
         public string Password { get; set; }
 
         [Required]
